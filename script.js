4ace1faf5dc6b69bb34bc964a68bf0c2122eb0c7
nelsonferrucho <nelson.ferrucho@gmail.com>
2022-06-25 23:55:12 -0500

    clase022 ok

--- script.js
+++ script.js
@@ -13,7 +13,24 @@ const createTask = (evento) =>{
     evento.preventDefault();
     console.log(evento);
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    // atrapar el input
+    const value = input.value;
+    
+    task = document.querySelector("[data-task]");
+    input.value = "";
+    
+    //backticks : pegamos el <div> de lo nuevo a tratar
+    //replazamo el texto por value como valor nuevo
+    const content = `<div>
+       <i class="far fa-check-square icon"></i>
+       <span class="task">${value}</span>
+       </div>
+       <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //ajustando el nuevo div al html
+    task.innerHTML = content;
+
+    console.log(task);
+
 }
 
 console.log(btn);
