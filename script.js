d3c46569e0ebd89f135b5cc81599e123bf513528
nelsonferrucho <nelson.ferrucho@gmail.com>
2022-06-26 01:25:49 -0500

    clase4 ok

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const btn = document.querySelector("[data-form-btn]");
+// IIFE : para encapsular las funciones creadas
+// y que no se vean en consola
 
-console.log(btn);
+( () => {
+const btn = document.querySelector('[data-form-btn]');
+
 
 //pasandolo a funcion flecha
 //btn.addEventListener("click", function(evento){
@@ -8,39 +11,67 @@ console.log(btn);
 //    console.log(evento);
 //    
 
+
 // usando metodos para llamado a la funcion 
 const createTask = (evento) =>{
     evento.preventDefault();
-    console.log(evento);
-    const input = document.querySelector("[data-form-input]");
+
+    const input = document.querySelector('[data-form-input]');
     // atrapar el input
     const value = input.value;
-
-    const list = document.querySelector("[data-list]");
-    
-    const task = document.createElement("li");
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
     
     //adicionando la clase que forma el entorno del item
     task.classList.add('card');
-    input.value = "";
-    
+    input.value = '';
+
+    //creando <div> a partir de codigo
+    const taskContent = document.createElement('div');
+
+    const titleTask = document.createElement('span');
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+
     //backticks : pegamos el <div> de lo nuevo a tratar
     //replazamo el texto por value como valor nuevo
-    const content = `<div>
-       <i class="far fa-check-square icon"></i>
-       <span class="task">${value}</span>
-       </div>
+    const content = `
        <i class="fas fa-trash-alt trashIcon icon"></i>`;
     //ajustando el nuevo div al html
-    task.innerHTML = content;
-
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-    console.log(task);
 
-}
+};
 
-console.log(btn);
 
-btn.addEventListener("click",createTask);
 
-//
+btn.addEventListener("click", createTask);
+
+//funcion para tomar las clases del <div> 
+const checkComplete = () => {
+    const i = document.createElement('i'); 
+    i.classList.add('far','fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+};
+
+const completeTask = (event) =>{
+    
+    //agregando una nueva clase
+    const element = event.target;
+    //element.classList.add('fas');
+    //element.classList.remove(far);
+
+    //otra forma es con toggle
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+
+
+};
+})();
+
